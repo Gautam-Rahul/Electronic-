316dc8b24d9c55906506af6048d7a383b1e31a31
Gautam-Rahul <rahulbhumika99@gmail.com>
2024-12-28 13:48:02 +1100

    Project officially started

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>Copyright © 2024 rahulgautam.com.np</div>
+  )
+}
+
+export default Footer
